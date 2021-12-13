b5984302c10254dd6aa9368e5c91b64e6b1eb9a0
Ali Hassnain <ali1397hasnain@gmail.com>
2021-12-13 01:58:13 -0800

    Complete To Do list

--- app.js
+++ app.js
@@ -3,42 +3,51 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.get("/", function(req, res){
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    const today = new Date();
+    const currentDay = today.getDay();
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    const day = today.toLocaleDateString("en-US", options);
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-    
-        default:
-            console.log("The current Day is " + currentDay);
-            break;
-    }
-
-    res.render("list" , {kindOfDay: day});
+    res.render("list" , {listTitle: day, newListItems: items});
 });
+
+app.post("/", function(req, res){
+    console.log(req.body);
+    const item = req.body.newItem;
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function(req, res){
+    res.render("list" , {listTitle: "Work List", newListItems: workItems});
+ });
+
+app.post("/work", function(req, res){
+    const item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work"); 
+ });
+
+
+ app.get("/about" , function(req,res){
+    res.render("about");
+ });
 
 app.listen(3000, function(){
     console.log("Port working");
